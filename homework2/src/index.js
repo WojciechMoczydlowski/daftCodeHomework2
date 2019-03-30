514d2c73d6b5c167861b7b0f269ebc93c4049337
Wojtek M <wojciech.moczydlowski98@gmail.com>
2019-03-30 19:37:52 +0100

    sass config

---
+++ homework2/src/index.js
@@ -0,0 +1,7 @@
+import './styles/style.css'
+import 'normalize.css'; 
+import './styles/main.sass'
+
+import myScript from './js/script';
+
+myScript();
---
+++ homework2/webpack.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    isProduction
+                        ? MiniCssExtractPlugin.loader
+                        : { loader: 'style-loader', options: { sourceMap: true } },
+                    { loader: 'css-loader', options: { sourceMap: isProduction } },
+                    { loader: 'postcss-loader', options: { sourceMap: isProduction } },
+                    { loader: 'sass-loader', options: { sourceMap: isProduction } },
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new HtmlWebpackPlugin()
+    ]
+};
